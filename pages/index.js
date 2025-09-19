aca7e5d70c4f35e549f1371f797218bacefb7163
Sergius Fortuna <54817115+sfortuna77@users.noreply.github.com>
2025-09-19 12:52:34 +0000

    adiciona .pages inicial

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home() {
+    return <h1>Este é a primeira mensagem de uma página criada no TabNews com Felipe Deschamps</h1>
+}
+
+export default Home;
---
+++ pages/produtos/index.js
@@ -0,0 +1,5 @@
+function Produtos() {
+    return <h1>Esta é uma página de Produtos que foi criada para ver como realmente funciona.</h1>
+}
+
+export default Produtos;
