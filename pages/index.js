6540405f9d241c9a93ecc3ad892da9e455ec1519
Sergius Fortuna <54817115+sfortuna77@users.noreply.github.com>
2025-09-29 14:38:47 +0000

    config: add `prettier` and vscode config

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-  return <h1>Este é a primeira mensagem de uma página criada no TabNews com Felipe Deschamps, onde estou aprendendo sobre NODE.JS</h1>
+  return (
+    <h1>
+      Este é a primeira mensagem de uma página criada no TabNews com Felipe
+      Deschamps, onde estou aprendendo sobre NODE.JS
+    </h1>
+  );
 }
 
 export default Home;
